2a0d9a351e073992f9127b92f66d0596e44ba850
kjm9547 <woals8115@naver.com>
2022-02-08 00:34:21 +0900

    fighting

---
+++ screens/CalendarScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+function CalendarScreen(){
+    return(
+        <View style={styles.block}/>
+    )
+}
+
+const styles = StyleSheet.create({
+    block:{}
+})
+
+export default CalendarScreen;
---
+++ screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+function SearchScreen(){
+    return(
+        <View style={styles.block}/>
+    )
+}
+
+const styles = StyleSheet.create({
+    block:{}
+})
+
+export default SearchScreen;
