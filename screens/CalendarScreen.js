0bd56ed8cdaf6b07d707bebf71d2cbca18f0424b
kjm9547 <woals8115@naver.com>
2022-02-21 22:24:05 +0900

    qweq

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,17 +1,57 @@
-import React, { useContext } from "react";
-import { StyleSheet, View, Text} from "react-native";
+import React, { useRef, useState, useEffect} from "react";
+import { StyleSheet, View, Animated,Button} from "react-native";
+function FadeInAndOut(){
+    const animation = useRef(new Animated.Value(1)).current
+    const [enabled,setEnabled] = useState(false)
 
+    useEffect(()=>{
+        Animated.timing(animation,{
+            toValue: enabled? 1:0,
+            useNativeDriver: true
+        }).start()
+        }, [enabled, animation]
+    )
+
+    return(
+        <View>
+            <Animated.View
+                style={[styles.rectangle,
+                {
+                    transform:[
+                        {
+                            translateX:animation.interpolate({
+                                inputRange: [0,1],
+                                outputRange:[0, 150]
+                            })
+                        }
+                    ],
+                    opacity: animation.interpolate({
+                        inputRange: [0,1],
+                        outputRange: [1,0]
+                    })
+                }
+                ]}
+            />
+            <Button
+                title="Toggle"
+                onPress={()=>{setEnabled(!enabled)}}
+                    
+            />
+        </View>
+    )
+}
 function CalendarScreen(){
     
     return(
         <View style={styles.block}>
-           
+           <FadeInAndOut></FadeInAndOut>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     block:{},
+    rectangle: {width:100, height:100, backgroundColor:'black'}
    
 });
 
